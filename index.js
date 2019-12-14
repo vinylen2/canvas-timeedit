e409fc26f6cdcc41d4e13096d7a77cace9fccdf1
vinylen2 <gabrielwallen93@gmail.com>
2019-12-14 11:12:33 +0100

    fixed canvas

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const timeedit = require('./api/timeedit.js');
+const timeedit = require('./api/timeedit');
+const canvas = require('./api/canvas');
 
-const result = timeedit.get().then((response) => {
-  console.log(response.data.reservations);
-});
+// const path = 'courses/8176/calendar_events/timetable';
+const path = 'calendar_events?context_codes[]=group_54297&start_date=2019-11-24&end_date=2020-01-05&per_page=50';
+
+
+canvas.get(path)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch(error => console.log(error.response.data));
