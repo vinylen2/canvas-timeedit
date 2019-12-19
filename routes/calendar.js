81c761ef85647da53df1adff40ba1c0c660da94f
vinylen2 <gabrielwallen93@gmail.com>
2019-12-19 16:36:32 +0100

    done

--- routes/calendar.js
+++ routes/calendar.js
@@ -3,29 +3,56 @@ const createCalendarEvent = require ('../constructors/calendarEvent.js');
 
 const canvas = require('../api/canvas');
 
+
+
 async function postEvents(ctx) {
   const reservations = ctx.request.body.postData;
 
-  reservations.forEach((reservation) => {
-    let calendarEvent = createCalendarEvent(reservation, 'group_54297')
-    canvas.post('calendar_events',calendarEvent)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch(error => console.log(error));
+  const responses = await reservations.map(async (reservation) => {
+    let calendar_event = createCalendarEvent(reservation, 'group_54297');
+    let status = '';
+
+    try {
+      status = await canvas.post('calendar_events', {calendar_event});
+      console.log(status);
+      return status;
+    }
+    catch {
+      return false;
+    }
+    canvas.post('calendar_events', {calendar_event}).then((result) => {
+      status = result;
+      status = true;
+    }).catch((error) => {
+      status = false;
+    });
   });
+
+  ctx.body = {
+    responses,
+  }
 }
 
 async function postEvent(ctx) {
   const reservation = ctx.request.body;
 
-  let calendarEvent = createCalendarEvent(reservation, 'group_54297');
+  let calendar_event = createCalendarEvent(reservation, 'group_54297');
+  let status = false;
+  let message = '';
 
-  canvas.post('calendar_events', {calendarEvent})
-    .then((response) => {
-      console.log(response);
-    })
-    .catch(error => console.log(error.response.data));
+  try {
+    const response = await canvas.post('calendar_events', {calendar_event});
+    status = true;
+    message = 'Lektion publicerad i Canvas';
+  } catch (error) {
+    status = false;
+    message = 'Lektion kunde inte publiceras i Canvas';
+  }
+
+  ctx.body = {
+    status,
+    message,
+  };
 }
 
 router.post('/events', postEvents);
