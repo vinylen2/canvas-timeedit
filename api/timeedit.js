9472f1105239afc9b6852ab0dc89b5334194dbe1
vinylen2 <gabrielwallen93@gmail.com>
2020-01-08 18:21:50 +0100

    added success

--- api/timeedit.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const URL = 'https://cloud.timeedit.net/ltu/web/schedule1/ri1YQ25Yy06Z47Q3g55Y50595605Q3g6764Z640QX170.json';
-
-module.exports = axios.create({
-  baseURL: URL,
-  withCredentials: true,
-});
--- routes/calendar.js
+++ routes/calendar.js
@@ -3,45 +3,16 @@ const createCalendarEvent = require ('../constructors/calendarEvent.js');
 
 const canvas = require('../api/canvas');
 
-
-
-async function postEvents(ctx) {
-  const reservations = ctx.request.body.postData;
-
-  const responses = await reservations.map(async (reservation) => {
-    let calendar_event = createCalendarEvent(reservation, 'group_54297');
-    let status = '';
-
-    try {
-      status = await canvas.post('calendar_events', {calendar_event});
-      console.log(status);
-      return status;
-    }
-    catch {
-      return false;
-    }
-    canvas.post('calendar_events', {calendar_event}).then((result) => {
-      status = result;
-      status = true;
-    }).catch((error) => {
-      status = false;
-    });
-  });
-
-  ctx.body = {
-    responses,
-  }
-}
-
 async function postEvent(ctx) {
   const reservation = ctx.request.body;
 
   let calendar_event = createCalendarEvent(reservation, 'group_54297');
   let status = false;
+  let response = null;
   let message = '';
 
   try {
-    const response = await canvas.post('calendar_events', {calendar_event});
+    response = await canvas.post('calendar_events', {calendar_event});
     status = true;
     message = 'Lektion publicerad i Canvas';
   } catch (error) {
@@ -50,12 +21,12 @@ async function postEvent(ctx) {
   }
 
   ctx.body = {
+    response: response.data,
     status,
     message,
   };
 }
 
-router.post('/events', postEvents);
 router.post('/event', postEvent);
 
 module.exports = router;
